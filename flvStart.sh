490d9092d84205048b935b448d28b6ccbfcd42f7
Harvey <laozhaochaguan@gmail.com>
2019-08-28 14:18:23 +0800

    Update flvStart.sh

--- flvStart.sh
+++ flvStart.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 /livego &
-ffmpeg -re -stream_loop -1  -i demo.mov  -acodec aac  -vcodec libx264   -f flv   rtmp://0.0.0.0:1935/demo/demo.flv
+ffmpeg -re -stream_loop -1 -i demo.mov  -acodec aac -vcodec libx264 -profile:v baseline -level 3.0 -f flv rtmp://0.0.0.0:1935/demo/demo.flv
 
 exec "$@"
